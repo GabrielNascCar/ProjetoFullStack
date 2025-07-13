4ae40d1992157310e1e5c05db06b505b0a1986b7
Gabriel do nascimento carvalho <nascimentocarvalho894@gmail.com>
2025-07-13 19:45:06 -0300

    Projeto criado

---
+++ GerenciadorCliente/src/test/java/org/desafio/gerenciadorcliente/GerenciadorClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.desafio.gerenciadorcliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorClienteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
