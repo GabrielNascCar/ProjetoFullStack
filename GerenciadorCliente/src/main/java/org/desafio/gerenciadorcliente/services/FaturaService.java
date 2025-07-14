01a5e5ceacc9cbd1e3cb54725a2e75fbe531e86d
Gabriel do nascimento carvalho <nascimentocarvalho894@gmail.com>
2025-07-14 00:54:54 -0300

    feat(fatura): atualiza status da fatura e bloqueia cliente em atraso

--- GerenciadorCliente/src/main/java/org/desafio/gerenciadorcliente/services/FaturaService.java
+++ GerenciadorCliente/src/main/java/org/desafio/gerenciadorcliente/services/FaturaService.java
@@ -1,10 +1,15 @@
 package org.desafio.gerenciadorcliente.services;
 
+import org.desafio.gerenciadorcliente.model.Cliente;
 import org.desafio.gerenciadorcliente.model.Fatura;
+import org.desafio.gerenciadorcliente.repositories.ClienteRepository;
 import org.desafio.gerenciadorcliente.repositories.FaturaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 @Service
 public class FaturaService {
@@ -13,11 +18,41 @@ public class FaturaService {
     @Autowired
     private FaturaRepository faturaRepository;
 
+    @Autowired
+    private ClienteRepository clienteRepository;
+
     @Transactional
     public Fatura salvarFatura(Fatura fatura) {
-        fatura.setStatus("ABERTA");
+        atualizarStatusFatura(fatura);
         faturaRepository.save(fatura);
         return fatura;
+    }
+
+    private void atualizarStatusFatura(Fatura fatura) {
+        if ("PAGO".equals(fatura.getStatus())) {
+            return;
+        }
+
+        if (LocalDate.now().isAfter(fatura.getDataVencimento())) {
+            long diasAtraso = ChronoUnit.DAYS.between(fatura.getDataVencimento(), LocalDate.now());
+
+            if (diasAtraso > 0) {
+                fatura.setStatus("ATRASADA");
+
+                if (diasAtraso > 3) {
+                    bloquearCliente(fatura.getCliente());
+                }
+            }
+        } else {
+            fatura.setStatus("ABERTA");
+        }
+    }
+
+    public void bloquearCliente(Cliente cliente) {
+        Cliente cliente1 = clienteRepository.findById(cliente.getId()).orElseThrow();
+        cliente1.setStatusBloqueio("BLOQUEADO");
+        cliente1.setLimiteCredito(0.0);
+        clienteRepository.save(cliente1);
     }
 
 
